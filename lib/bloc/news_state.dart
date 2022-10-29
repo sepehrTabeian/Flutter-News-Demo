713456eb3c5ade4ffa3babe84845c0850ca0e374
Sepehr Tabeian <sepehrtabeian@gmail.com>
2022-10-30 00:44:39 +0330

    refactor : state class extends from equitable

--- lib/bloc/news_state.dart
+++ lib/bloc/news_state.dart
@@ -1,15 +1,25 @@
+import 'package:equatable/equatable.dart';
 import 'package:sample/data/NewsModel.dart';
 
-abstract class NewsState{}
+abstract class NewsState extends Equatable{
+  @override
+  List<Object?> get props => [];
+}
 class InitialNews extends NewsState{}
 class LoadingNews extends NewsState{}
 class LoadedNews extends NewsState{
   final NewsModel newsModel;
 
   LoadedNews({required this.newsModel});
+
+  @override
+  List<Object?> get props => [newsModel];
+
 }
 class FailedNews extends NewsState{
   final String message;
 
   FailedNews({required this.message});
+  @override
+  List<Object?> get props => [message];
 }
